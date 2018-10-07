20f16485c061caedbbbb2f2dedab869165939299
xXAntonioXx <jose_antony11@hotmail.com>
2018-10-06 22:41:31 -0700

    rutina

--- ToDo/resources/views/welcome.blade.php
+++ ToDo/resources/views/welcome.blade.php
@@ -5,6 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{csrf_token()}}">
+        <link rel="stylesheet" href="{{ asset('/css/CSS.css') }}">
         <script>window.Laravel = {csrfToken:'{{csrf_token()}}'}</script>
 
         <title>Cosas por hacer</title>
@@ -17,8 +18,8 @@
         </style>
     </head>
     <body style="margin:0">
-        <nav>
-        <a href="/salir">salir</a>
+        <nav class="navi">
+            <a href="/salir">salir</a>
         </nav>
         <div id="main"> 
             <principal></principal>
